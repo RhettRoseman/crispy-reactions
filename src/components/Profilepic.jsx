a5d6000969ebe25c6449f5247a075612dd3fcae7
RhettRoseman <rroseman@rcplanners.com>
2023-11-29 13:11:30 -0500

    profile-pic added

---
+++ src/components/Profilepic.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const imageUrl="images/profile.jpeg";
+
+ export default function ProfilePic () {
+  return (
+    <div className="profile-pic">
+      <img src="../../images/profile.jpg" alt="profile" />
+    </div>
+  );
+};
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import "./styles/home.css"
+import "./styles/home.css";
+// import ProfilePic from "../components/profilepic";
+// import ProfilePic from "../components/profilepic";
+
+
+
 export default function Home() {
   return (
     <div className="home">
+      <div className="profile"> <img src="../../images/profile.jpg" alt="" /> </div>
       <h1>JR3 Portfolio Project</h1>
      
     </div>
